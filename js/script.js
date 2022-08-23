9efb9bfdae0c5b99c798180421bcb8a910dc9324
Bohdan <bogdanbrohovetsky@gmail.com>
2022-08-23 07:35:40 +0300

    dib float layout

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*(50==="50") ? console.log("yes") : console.log("no");
+switch (49) {
+	case 49:
+	console.log("it's 49");
+	break;
+	case 100:
+	console.log("it's 100");
+	break;
+	default:
+	console.log("fuck off");
+}*/
+
+for( let i = 1; i < 11; i++) {
+		if (i === 6) {
+			continue;
+		}
+			console.log(i);
+}
